cd614296dff3f283218ec94b827659d9d2d1f422
Sakshi <panwarsakshi28@gmail.com>
2022-08-16 13:45:48 +0530

    Second Commit

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./login.css";
 import Signup from "./SignUp";
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 import { createButton } from "react-social-login-buttons";
 
